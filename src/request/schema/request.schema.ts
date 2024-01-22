8f6b16e7731df53a827edfef76cad193ae5a8e6e
DityaIlhami28 <dityailhami@gmail.com>
2024-01-22 21:00:29 +0700

    get request by id

--- src/request/schema/request.schema.ts
+++ src/request/schema/request.schema.ts
@@ -6,14 +6,48 @@ import { Document, Types } from 'mongoose';
   versionKey: false,
 })
 export class Request extends Document {
-  @Prop()
+  @Prop({ required: true })
   title: string;
-  @Prop()
-  donorRequired: number;
-  @Prop()
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ type: Types.ObjectId, required: true })
   hospitalId: Types.ObjectId;
-  @Prop()
-  bloodType: string;
+
+  @Prop({
+    type: {
+      A: {
+        request: { type: Number, default: 0 },
+        collected: { type: Number, default: 0 },
+      },
+      B: {
+        request: { type: Number, default: 0 },
+        collected: { type: Number, default: 0 },
+      },
+      AB: {
+        request: { type: Number, default: 0 },
+        collected: { type: Number, default: 0 },
+      },
+      O: {
+        request: { type: Number, default: 0 },
+        collected: { type: Number, default: 0 },
+      },
+    },
+    default: {
+      A: { request: 0, collected: 0 },
+      B: { request: 0, collected: 0 },
+      AB: { request: 0, collected: 0 },
+      O: { request: 0, collected: 0 }
+    },
+  })
+  bloodType: Record<string, { request: number; collected: number }>;
+
+  @Prop({ type: Number, default: 0 })
+  totalRequest: number;
+
+  @Prop({ type: Number, default: 0 })
+  totalCollected: number;
 }
 
 export const RequestSchema = SchemaFactory.createForClass(Request);
